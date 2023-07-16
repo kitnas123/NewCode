349870f702d926b93275fa8343643bc11f554ebf
kitnas123 <139665392+kitnas123@users.noreply.github.com>
2023-07-16 11:35:10 +0800

    Add files via upload

---
+++ service/nav.php
@@ -0,0 +1,35 @@
+<nav>
+    <div class="wrapper">
+      <div class="logo">
+        <a href="#">
+          <img class="img" src="../images/jedz.png" alt="Logo">
+          Jedz Aesthetic Clinic
+        </a>
+      </div>
+      <input type="radio" name="slider" id="menu-btn">
+      <input type="radio" name="slider" id="close-btn">
+      <ul class="nav-links">
+        <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="landing.php">Shop</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li>
+          <a href="#" class="desktop-item">Team</a>
+          <!--<input type="checkbox" id="showDrop">
+          <label for="showDrop" class="mobile-item">Dropdown Menu</label>
+          <ul class="drop-menu">
+            <li><a href="#">Drop menu 1</a></li>
+            <li><a href="#">Drop menu 2</a></li>
+            <li><a href="#">Drop menu 3</a></li>
+            <li><a href="#">Drop menu 4</a></li>
+          </ul>-->
+        </li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+      <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
+    </div>
+  </nav>
+
+</body>
+
+</html>
